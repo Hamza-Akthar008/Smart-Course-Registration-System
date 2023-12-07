92f087cbfe51416af43ca44aa762bf63ae804e5b
mumhammad-abdullah <f201027@cfd.nu.edu.pk>
2023-12-07 23:48:11 +0500

    Manage HOD Controller CRUD SERVER SIDE

---
+++ server/controller/managehod_controller.js
@@ -0,0 +1,95 @@
+// controllers/hodController.js
+
+import HOD from "../models/hod.js";
+
+
+
+export const addNewHOD = async (req, res) => {
+  try {
+    // Destructure the required fields from the request body
+    const { HODID, depart_id, Hod_name, hod_email, hod_contact, hod_password } = req.body;
+console.log(req.body);
+    // Check if an HOD with the given email already exists
+    const existingHOD = await HOD.findOne({
+      where: { hod_email },
+    });
+
+    if (existingHOD) {
+      return res.status(400).json({ success: false, error: 'HOD with this email already exists' });
+    }
+
+    // Create a new HOD record in the database
+    const newHOD = await HOD.create({
+        HODID, depart_id, Hod_name, hod_email, hod_contact, hod_password,
+    });
+
+    res.status(201).json({ success: true, data: newHOD });
+  } catch (error) {
+    console.error('Error adding new HOD:', error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
+  }
+};
+
+
+export const getAllHODs = async (req, res) => {
+  try {
+    // Fetch all HOD records from the database
+    const allHODs = await HOD.findAll({
+      attributes: ['HODID', 'depart_id', 'Hod_name', 'hod_contact', 'hod_email'], // List of fields to retrieve
+    });
+console.log(allHODs);
+    res.status(200).json({ success: true, data: allHODs });
+  } catch (error) {
+    console.error('Error retrieving HODs:', error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
+  }
+};
+
+export const deleteHODById = async (req, res) => {
+  try {
+    const { HODID } = req.body;
+
+    // Find the HOD by ID
+    const hodToDelete = await HOD.findByPk(HODID);
+console.log(hodToDelete);
+    // If the HOD with the specified ID doesn't exist, return an error
+    if (!hodToDelete) {
+      return res.status(404).json({ success: false, error: 'HOD not found' });
+    }
+
+    // Delete the HOD
+    await hodToDelete.destroy();
+
+    res.status(200).json({ success: true, message: 'HOD deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting HOD:', error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
+  }
+};
+export const editHODById = async (req, res) => {
+  try {
+    const { HODID, depart_id, Hod_name, hod_contact, hod_email } = req.body;
+
+    // Find the HOD by ID
+    const hodToUpdate = await HOD.findByPk(HODID);
+
+    // If the HOD with the specified ID doesn't exist, return an error
+    if (!hodToUpdate) {
+      return res.status(404).json({ success: false, error: 'HOD not found' });
+    }
+
+    // Update the HOD fields
+    hodToUpdate.depart_id = depart_id;
+    hodToUpdate.Hod_name = Hod_name;
+    hodToUpdate.hod_contact = hod_contact;
+    hodToUpdate.hod_email = hod_email;
+
+    // Save the changes
+    await hodToUpdate.save();
+
+    res.status(200).json({ success: true, message: 'HOD updated successfully' });
+  } catch (error) {
+    console.error('Error updating HOD:', error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
+  }
+};
