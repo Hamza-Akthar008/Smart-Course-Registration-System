83bafe4a94b254f4c8888cd05cbcd722d4d98be5
Hamza-Akthar008 <f200319@cfd.nu.edu.pk>
2024-03-22 23:51:37 +0500

    Config

---
+++ server/config/dbconfig.js
@@ -0,0 +1,20 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize({
+  dialect: "mysql",
+  username: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+});
+
+const dbconfig = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+
+  export { sequelize, dbconfig };
