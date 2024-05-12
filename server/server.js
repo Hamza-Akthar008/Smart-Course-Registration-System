0f50770d6a5756ea591ccb1d4f6290593310f598
Hamza-Akthar008 <75743423+Hamza-Akthar008@users.noreply.github.com>
2024-05-12 12:07:44 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -39,9 +39,14 @@ import { string } from "yup";
 //Mongoose Connect
 //rest object
 const app = express()
-app.use(cors({
-  origin: '*'
-}));
+app.use((req, res, next) => {
+ 
+  // You may need to adjust other CORS headers depending on your needs
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 await dbconfig();
 await syncModels();
 
